1846c0b9d4c4fffb5ce9c0041a4fd5c651145b33
Joseph Lumbley <joelumbley16@gmail.com>
2021-02-22 16:15:59 -0600

    Cleaned up code

--- DC_Testbed_2/Form1.vb
+++ DC_Testbed_2/Form1.vb
@@ -4,7 +4,7 @@
 'A work in progress...
 'This is a simple action role-playing game in which the hero navigates a labyrinth,
 'battles various monsters, avoids traps, solves puzzles, and loots any treasure that is found.
-'Coded by Joseph Lumbley.
+'Coded by Joseph Lumbley. Copyright 2021
 
 Imports System.Math
 Imports System.Drawing
