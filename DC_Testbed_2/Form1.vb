a798b4fcbadfe8c8e939c06d3d708cd072765900
Joseph Lumbley <joelumbley16@gmail.com>
2021-03-09 13:16:28 -0600

    Todays Commit 3/9/21

--- DC_Testbed_2/Form1.vb
+++ DC_Testbed_2/Form1.vb
@@ -4,13 +4,31 @@
 'A work in progress...
 'This is a simple action role-playing game in which the hero navigates a labyrinth,
 'battles various monsters, avoids traps, solves puzzles, and loots any treasure that is found.
-'Coded by Joseph Lumbley. Copyright 2021
+
+'MIT License
+'Copyright(c) 2021 Joseph Lumbley
+
+'Permission Is hereby granted, free Of charge, to any person obtaining a copy
+'of this software And associated documentation files (the "Software"), to deal
+'in the Software without restriction, including without limitation the rights
+'to use, copy, modify, merge, publish, distribute, sublicense, And/Or sell
+'copies of the Software, And to permit persons to whom the Software Is
+'furnished to do so, subject to the following conditions:
+
+'The above copyright notice And this permission notice shall be included In all
+'copies Or substantial portions of the Software.
+
+'THE SOFTWARE Is PROVIDED "AS IS", WITHOUT WARRANTY Of ANY KIND, EXPRESS Or
+'IMPLIED, INCLUDING BUT Not LIMITED To THE WARRANTIES Of MERCHANTABILITY,
+'FITNESS FOR A PARTICULAR PURPOSE And NONINFRINGEMENT. IN NO EVENT SHALL THE
+'AUTHORS Or COPYRIGHT HOLDERS BE LIABLE For ANY CLAIM, DAMAGES Or OTHER
+'LIABILITY, WHETHER In AN ACTION Of CONTRACT, TORT Or OTHERWISE, ARISING FROM,
+'OUT OF Or IN CONNECTION WITH THE SOFTWARE Or THE USE Or OTHER DEALINGS IN THE
+'SOFTWARE.
 
 Imports System.Math
-'Imports System.Drawing
 Imports System.Drawing.Drawing2D
 Imports System.Runtime.InteropServices
-'Imports System.ComponentModel
 Imports System.IO
 
 Public Structure HeroInfo
@@ -4055,6 +4073,8 @@ Public Enum MCI_NOTIFY As Integer
 End Enum
 
 Public Class GameSounds
+    'Game sounds class By IronRazer - https://www.dreamincode.net/forums/topic/378353-how-to-play-2-sounds-simultaneously/
+
     Inherits NativeWindow
 
     Public Event SoundEnded(ByVal SndName As String)
